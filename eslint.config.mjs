71189db9c3f780ad8900ab8f513b5fc8710b668f
PongThiti <76689893+swatcat1412@users.noreply.github.com>
2026-08-06 12:29:04 +0700

    Upload eslint.config.mjs

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const filename = fileURLToPath(import.meta.url);
+const directory = dirname(filename);
+const compat = new FlatCompat({ baseDirectory: directory });
+
+const eslintConfig = [
+  { ignores: [".next/**", ".next-build/**", "node_modules/**", "out/**"] },
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+];
+
+export default eslintConfig;
+
